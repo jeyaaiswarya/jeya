14dacbd15107a56b6e8b6c487c7dc54478ba3a66
jeyaaiswarya <jeyaaiswarya@gmail.com>
2017-12-16 22:50:28 +0530

    Create greater.py

---
+++ greater.py
@@ -0,0 +1,15 @@
+a=23
+b=45
+c=78
+if(a>b):
+	if(a>c):
+		print("a is greater ")
+	else:
+		print("c is greater")
+elif(b>a):
+	if(b>c):
+		print("b is greater " )
+	else:
+	    print("c is greater")
+else:
+	print("c is greater")
