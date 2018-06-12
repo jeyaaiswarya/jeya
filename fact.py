1d305ca57151d249fa9fd45dcb3779dc0b0f4335
jeyaaiswarya <jeyaaiswarya@gmail.com>
2018-06-12 09:34:31 +0530

    Update fact.py

--- fact.py
+++ fact.py
@@ -1,16 +1,9 @@
-def main():
-    i=1
-    fact=1
-    n=input("Enter the number")
-    if(n==0):
-        fact=1
-        print(fact)
-    else:
-        for i in range(1,n+1):
-            fact*=i
-        print("The factorial is")
-        print(fact)
-
-
-if __name__ == '__main__':
-    main()
+i=1
+fact=1
+n=int(input())
+if(n==0):
+	print(1)
+else:
+	for i in range(1,n+1):
+		fact*=i
+	print(fact)
