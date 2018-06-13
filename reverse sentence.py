e56d72449200823318bb7fecbffe9ebee80a9427
jeyaaiswarya <jeyaaiswarya@gmail.com>
2018-06-13 06:42:12 +0000

    Update reverse sentence.py

--- reverse sentence.py
+++ reverse sentence.py
@@ -6,7 +6,7 @@ data = alphabet.split() #split string into a list
 n=len(data)
 for i in range(n):
 	if i%2==0:
-		print(data[i])
-	else:
 		temp=data[i]
 		print(temp[::-1])
+	else:
+		print(data[i])
